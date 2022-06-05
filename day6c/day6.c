99d5581dde447f173b7d0b0159d0d3d03675168f
dreambucket13 <carmichr@gmail.com>
2022-06-05 07:20:16 -0400

    style, added assert

--- day6c/day6.c
+++ day6c/day6.c
@@ -1,57 +1,56 @@
-#include<stdio.h>
-
 /*
-Fish with initial timer of 0 will spawn 7 fish
-Fish with initial timer of 1 will spawn 7 fish
-Fish with initial timer of 2 will spawn 5 fish
-Fish with initial timer of 3 will spawn 5 fish
-Fish with initial timer of 4 will spawn 4 fish
-Fish with initial timer of 5 will spawn 4 fish
-Grand total: 26
+Chris Carmine
+AoC 2021 Day 6
 */
 
-unsigned long long multiply(int,int);
+#include <stdio.h>
+#include <assert.h>
+
+#define discreteTimers 6
+
+__UINT64_TYPE__ multiply(int timer,int daysLeft);
 
 int main() {
 
-    unsigned long long grandtotal = 0;
-    unsigned long long subtotal = 0;
-    unsigned long long spawn[] = {0,0,0,0,0,0};
-    int initialtimer = 0;
-    int daysleft = 256;
-    int startingfish[] = {1,2,5,1,1,4,1,5,5,5,3,4,1,2,2,5,3,5,1,3,4,1,5,2,5,1,4,1,2,2,1,5,1,1,1,2,4,3,4,2,2,4,5,4,1,2,3,5,3,4,1,1,2,2,1,3,3,2,3,2,1,2,2,3,1,1,2,5,1,2,1,1,3,1,1,5,5,4,1,1,5,1,4,3,5,1,3,3,1,1,5,2,1,2,4,4,5,5,4,4,5,4,3,5,5,1,3,5,2,4,1,1,2,2,2,4,1,2,1,5,1,3,1,1,1,2,1,2,2,1,3,3,5,3,4,2,1,5,2,1,4,1,1,5,1,1,5,4,4,1,4,2,3,5,2,5,5,2,2,4,4,1,1,1,4,4,1,3,5,4,2,5,5,4,4,2,2,3,2,1,3,4,1,5,1,4,5,2,4,5,1,3,4,1,4,3,3,1,1,3,2,1,5,5,3,1,1,2,4,5,3,1,1,1,2,5,2,4,5,1,3,2,4,5,5,1,2,3,4,4,1,4,1,1,3,3,5,1,2,5,1,2,5,4,1,1,3,2,1,1,1,3,5,1,3,2,4,3,5,4,1,1,5,3,4,2,3,1,1,4,2,1,2,2,1,1,4,3,1,1,3,5,2,1,3,2,1,1,1,2,1,1,5,1,1,2,5,1,1,4};
+
+
+    __UINT64_TYPE__ grandTotal = 0;
+    __UINT64_TYPE__ subtotal = 0;
+    __UINT64_TYPE__ spawn[discreteTimers] = { 0 };
+    int initialTimer = 0;
+    int daysLeft = 256;
+    int startingFish[] = {1,2,5,1,1,4,1,5,5,5,3,4,1,2,2,5,3,5,1,3,4,1,5,2,5,1,4,1,2,2,1,5,1,1,1,2,4,3,4,2,2,4,5,4,1,2,3,5,3,4,1,1,2,2,1,3,3,2,3,2,1,2,2,3,1,1,2,5,1,2,1,1,3,1,1,5,5,4,1,1,5,1,4,3,5,1,3,3,1,1,5,2,1,2,4,4,5,5,4,4,5,4,3,5,5,1,3,5,2,4,1,1,2,2,2,4,1,2,1,5,1,3,1,1,1,2,1,2,2,1,3,3,5,3,4,2,1,5,2,1,4,1,1,5,1,1,5,4,4,1,4,2,3,5,2,5,5,2,2,4,4,1,1,1,4,4,1,3,5,4,2,5,5,4,4,2,2,3,2,1,3,4,1,5,1,4,5,2,4,5,1,3,4,1,4,3,3,1,1,3,2,1,5,5,3,1,1,2,4,5,3,1,1,1,2,5,2,4,5,1,3,2,4,5,5,1,2,3,4,4,1,4,1,1,3,3,5,1,2,5,1,2,5,4,1,1,3,2,1,1,1,3,5,1,3,2,4,3,5,4,1,1,5,3,4,2,3,1,1,4,2,1,2,2,1,1,4,3,1,1,3,5,2,1,3,2,1,1,1,2,1,1,5,1,1,2,5,1,1,4};
     printf("Starting...\n");
 
-    for (initialtimer = 0; initialtimer < 6; initialtimer++){
+    for (initialTimer = 0; initialTimer < discreteTimers; initialTimer++){
 
-        subtotal = 0;
-        subtotal = 1 + multiply(initialtimer+1,daysleft);
-
-        spawn[initialtimer] = subtotal;
-        printf("initial timer %d: %lld\n",initialtimer,subtotal);
+        subtotal = 1 + multiply(initialTimer+1,daysLeft);
+        spawn[initialTimer] = subtotal;
+        printf("initial timer %d: %lld\n",initialTimer,subtotal);
     
     }
 
-    for (int i = 0; i < sizeof(startingfish)/sizeof(int); i++){
+    for (int i = 0; i < sizeof(startingFish)/sizeof(startingFish[0]); i++){
 
-        grandtotal = grandtotal + spawn[startingfish[i]];
+        grandTotal += spawn[startingFish[i]];
     }
 
-    printf("Grand total: %lld\n",grandtotal);
+    printf("Grand total: %lld\n",grandTotal);
+    assert(grandTotal == 1617359101538);
 
     return 0;
 
 } //main
 
-unsigned long long multiply (int timer, int daysleft){
+__UINT64_TYPE__ multiply (int timer, int daysLeft){
 
     //base case
-    if (timer>daysleft){
+    if (timer>daysLeft){
         return 0;
     } else {
     //recursive case
-    daysleft = daysleft - timer;
-    return 1 + multiply(7,daysleft) + multiply(9,daysleft);
+    daysLeft -= timer;
+    return 1 + multiply(7,daysLeft) + multiply(9,daysLeft);
     
     }
 
