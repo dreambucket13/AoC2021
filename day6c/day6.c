08f1922902b47ebd36b8b4d848f6ae59117202cc
dreambucket13 <carmichr@gmail.com>
2022-06-05 08:54:49 -0400

    branch to memoize

--- day6c/day6.c
+++ day6c/day6.c
@@ -8,7 +8,7 @@ AoC 2021 Day 6
 
 #define discreteStartingTimers 6
 
-__UINT64_TYPE__ multiply(int timer,int daysLeft);
+__UINT64_TYPE__ multiply(int timer,int daysLeft, __UINT64_TYPE__ *resultCache[9][256]);
 
 int main() {
 
@@ -20,10 +20,12 @@ int main() {
     int startingFish[] = {1,2,5,1,1,4,1,5,5,5,3,4,1,2,2,5,3,5,1,3,4,1,5,2,5,1,4,1,2,2,1,5,1,1,1,2,4,3,4,2,2,4,5,4,1,2,3,5,3,4,1,1,2,2,1,3,3,2,3,2,1,2,2,3,1,1,2,5,1,2,1,1,3,1,1,5,5,4,1,1,5,1,4,3,5,1,3,3,1,1,5,2,1,2,4,4,5,5,4,4,5,4,3,5,5,1,3,5,2,4,1,1,2,2,2,4,1,2,1,5,1,3,1,1,1,2,1,2,2,1,3,3,5,3,4,2,1,5,2,1,4,1,1,5,1,1,5,4,4,1,4,2,3,5,2,5,5,2,2,4,4,1,1,1,4,4,1,3,5,4,2,5,5,4,4,2,2,3,2,1,3,4,1,5,1,4,5,2,4,5,1,3,4,1,4,3,3,1,1,3,2,1,5,5,3,1,1,2,4,5,3,1,1,1,2,5,2,4,5,1,3,2,4,5,5,1,2,3,4,4,1,4,1,1,3,3,5,1,2,5,1,2,5,4,1,1,3,2,1,1,1,3,5,1,3,2,4,3,5,4,1,1,5,3,4,2,3,1,1,4,2,1,2,2,1,1,4,3,1,1,3,5,2,1,3,2,1,1,1,2,1,1,5,1,1,2,5,1,1,4};
     printf("Starting...\n");
 
+    __UINT64_TYPE__ resultCache[discreteStartingTimers+3][daysLeft];
+
     for (initialTimer = 0; initialTimer < discreteStartingTimers; initialTimer++){
 
         //timers are plus 1 since 0 is a valid timer value
-        subtotal = 1 + multiply(initialTimer+1,daysLeft);
+        subtotal = 1 + multiply(initialTimer+1,daysLeft, &resultCache);
         spawn[initialTimer] = subtotal;
         printf("initial timer %d: %lld\n",initialTimer,subtotal);
     
@@ -41,7 +43,12 @@ int main() {
 
 } //main
 
-__UINT64_TYPE__ multiply (int timer, int daysLeft){
+__UINT64_TYPE__ multiply (int timer, int daysLeft, __UINT64_TYPE__ *resultCache[9][256] ){
+    
+    if (resultCache[timer][daysLeft] != 0){
+        return resultCache[timer][daysLeft];
+    }
+
 
     //base case
     if (timer>daysLeft){
@@ -50,7 +57,9 @@ __UINT64_TYPE__ multiply (int timer, int daysLeft){
     //recursive case
     daysLeft -= timer;
     //timers are plus 1 since 0 is a valid timer value, so 7 is 6+1 and 9 is 8+1
-    return 1 + multiply(7,daysLeft) + multiply(9,daysLeft);
+    __UINT64_TYPE__ subTotal = 1 + multiply(7,daysLeft,resultCache) + multiply(9,daysLeft,resultCache);
+    resultCache[timer][daysLeft] = subTotal;
+    return subTotal;
     
     }
 
